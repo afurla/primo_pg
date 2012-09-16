f743d073dbaaa30c6a0b97268d55b0a5436dece2
afurlani <afurla@chiantinet.net>
2012-09-16 06:30:15 -0400

    Inital commit on the first full PG project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PrimoPg::Application.config.secret_token = '9fa4cd22e55d9a10d7db6c3fcaccb5a13f40f8e6231bc425ae6db44de3c877cc12f37d83ccbf23dd8c2497cafe8ffb556a8c6d218b52da1426ffb7d6bb4efa27'
